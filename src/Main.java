db4b05b50036adfdf835e323db25e396813e4079
Myhtafa_break <sum686431@gmail.com>
2024-09-22 15:37:00 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int hak = 6;  // Kalan hak
-        String word = "orange";  // Tahmin edilmesi gereken kelime
+        int hak = 6;  
+        String word = "orange";  
         boolean istrue = true;
 
-        // Kelimenin baştaki durumu (örneğin "______")
         StringBuilder cizgiBuilder = new StringBuilder("______");
 
         Scanner sc = new Scanner(System.in);
@@ -14,31 +13,31 @@ public class Main {
         while (hak > 0) {
             System.out.println("Guess the word: " + cizgiBuilder);
 
-            // Kullanıcıdan bir karakter alınır
+            
             System.out.print("Bir karakter girin: ");
-            char karak = sc.next().charAt(0);  // İlk karakter alınır
+            char karak = sc.next().charAt(0); 
 
             boolean isFound = false;
             for (int i = 0; i < word.length(); i++) {
                 if (word.charAt(i) == karak) {
-                    cizgiBuilder.setCharAt(i, word.charAt(i));  // Doğru tahmin edilen harf yerleştirilir
+                    cizgiBuilder.setCharAt(i, word.charAt(i));  
                     isFound = true;
                 }
             }
 
-            // Eğer karakter yanlış tahmin edilirse hak azalır
+           
             if (!isFound) {
                 hak--;
                 System.out.println("Yanlış tahmin! Kalan hak: " + hak);
             }
 
-            // Eğer kelimenin tamamı doğru tahmin edildiyse oyun biter
+           
             if (cizgiBuilder.toString().equals(word)) {
                 System.out.println("Tebrikler, kelimeyi doğru tahmin ettiniz: " + word);
                 break;
             }
 
-            // Haklar biterse oyun kaybedilir
+         
             if (hak == 0) {
                 System.out.println("Maalesef kaybettiniz! Doğru kelime: " + word);
             }
